95ab0283071cf6c15d1e3c147efacfae89c11e1b
ANSELAS <andrius_scouts@yahoo.com>
2023-01-19 21:56:48 +0200

    Exercise three done

--- php_first_test/index.php
+++ php_first_test/index.php
@@ -1,22 +1,73 @@
 <?php
+/*
+3.    Masyve $holidays turime kelionių agentūros siūlomas keliones su kaina ir dalyvių skaičiumi.
+      Terminale išspausdinkite santrauką, kurioje matytusi miesto pavadinimas, kelionių pavadinimai
+      ir dalyvių sumokėta suma. Dėmesio! Santraukoje nerodykite tų kelionių, kurių kaina yra null. (3 balai)
 
-//2. Grąžinkite visų skaičių, esančių $numbers masyve, sandaugą (1 balas)
+      Destination "Paris".
+      Titles: "Romantic Paris", "Hidden Paris"
+      Total: 99500
+      ************
+      Destination "New York"
+     ...
+*/
 
-function exercise2(): int {
-
-    $numbers = [
-        [1, 3, 5],
-        [55, 87, 100],
-        [12],
-        [],
+function exercise3(): void {
+    $holidays = [
+        [
+            'title' => 'Romantic Paris',
+            'destination' => 'Paris',
+            'price' => 1500,
+            'tourists' => 55,
+        ],
+        [
+            'title' => 'Amazing New York',
+            'destination' => 'New York',
+            'price' => 2699,
+            'tourists' => 21,
+        ],
+        [
+            'title' => 'Spectacular Sydney',
+            'destination' => 'Sydney',
+            'price' => 4130,
+            'tourists' => 9,
+        ],
+        [
+            'title' => 'Hidden Paris',
+            'destination' => 'Paris',
+            'price' => 1700,
+            'tourists' => 10,
+        ],
+        [
+            'title' => 'Emperors of the past',
+            'destination' => 'Beijing',
+            'price' => null,
+            'tourists' => 10,
+        ],
     ];
-   $result = 1;
+    $holidaySummary = [];
 
-   foreach ($numbers as $number) {
-       foreach ($number as $value) {
-           $result *= $value;
-       }
-   }
-    return $result;
+    foreach ($holidays as $holiday) {
+        if ($holiday['price'] === null) {
+            continue;
+        }
+        $destination = $holiday['destination'];
+        if (!isset($holidaySummary[$destination])) {
+            $holidaySummary[$destination] = [
+                'destination' => $destination,
+                'titles' => [],
+                'total' => 0,
+            ];
+        }
+        $holidaySummary[$destination]['titles'][] = $holiday['title'];
+        $holidaySummary[$destination]['total'] += $holiday['price'] * $holiday['tourists'];
+    }
+
+    foreach ($holidaySummary as $summary) {
+        echo "Destination: " . $summary['destination'] . PHP_EOL .
+            "Titles: " . implode(', ', $summary['titles']). PHP_EOL .
+            "Total: " . $summary['total'] . PHP_EOL .
+            '************' . PHP_EOL;
+    }
 }
-var_dump(exercise2());
+exercise3();
